f014432fed0b96e6c2b9b546d140766353027ddb
Galactros <xtsjamal@gmail.com>
2025-01-22 16:35:17 -0300

    mmnm

--- nail/static/js/scripts.js
+++ nail/static/js/scripts.js
@@ -6,10 +6,22 @@ function fetchWorkloadPods() {
 
     $.get(`/workload-pods/?environment=${environment}&cluster=${cluster}&namespace=${namespace}&workload_name=${workload}`)
         .done((data) => {
-            // Render table...
+            let resultHtml = '<table class="table table-bordered">';
+            resultHtml += '<thead><tr><th>Pod Name</th><th>Status</th><th>Creation Time</th><th>Tag</th><th>Restarts</th></tr></thead><tbody>';
+            data.forEach(pod => {
+                resultHtml += `<tr>
+                    <td>${pod.pod_name}</td>
+                    <td>${pod.pod_status}</td>
+                    <td>${pod.creation_time}</td>
+                    <td>${pod.tag}</td>
+                    <td>${pod.restarts}</td>
+                </tr>`;
+            });
+            resultHtml += '</tbody></table>';
+            $("#workload-pods-result").html(resultHtml);
         })
         .fail((err) => {
-            // Handle errors...
+            $("#workload-pods-result").html(`<div class="alert alert-danger">${err.responseJSON.error}</div>`);
         });
 }
 
@@ -21,9 +33,21 @@ function fetchHPA() {
 
     $.get(`/hpa/?environment=${environment}&cluster=${cluster}&namespace=${namespace}&deployment_name=${deployment}`)
         .done((data) => {
-            // Render table...
+            let resultHtml = '<table class="table table-bordered">';
+            resultHtml += '<thead><tr><th>HPA Name</th><th>Min Replicas</th><th>Max Replicas</th><th>Current Replicas</th><th>Target CPU Utilization</th></tr></thead><tbody>';
+            data.forEach(hpa => {
+                resultHtml += `<tr>
+                    <td>${hpa.name}</td>
+                    <td>${hpa.min_replicas}</td>
+                    <td>${hpa.max_replicas}</td>
+                    <td>${hpa.current_replicas}</td>
+                    <td>${hpa.target_cpu_utilization_percentage}</td>
+                </tr>`;
+            });
+            resultHtml += '</tbody></table>';
+            $("#hpa-result").html(resultHtml);
         })
         .fail((err) => {
-            // Handle errors...
+            $("#hpa-result").html(`<div class="alert alert-danger">${err.responseJSON.error}</div>`);
         });
 }
